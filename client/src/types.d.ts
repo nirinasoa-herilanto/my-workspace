a120fcc58b23bf7266141c3ceadf9251ecf0e17a
Randriamananjara Nirinasoa Herilanto <nirinasoa.herilanto@gmail.com>
2023-12-13 14:42:24 +0300

    feat["client"]: add types file
    Used to customized application "type"

---
+++ client/src/types.d.ts
@@ -0,0 +1,29 @@
+import { ReactNode } from 'react';
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+/**
+ * ### ComponentPropsType
+ * A default component props type.
+ * 
+ * - Used to avoid `repetition` of declaring props `type` on each component.
+ * 
+ * Usage example
+ * ```jsx
+  import React from 'react';
+  import type { ComponentPropsType } from '@project/types';
+
+  const Layout: React.FC<ComponentPropsType> = ({ className, children }) => {
+    return (<div className={`layout ${className || ''}`}>
+      {children}
+    </div>);
+  };
+
+  export default Layout;
+
+ * ```
+ */
+declare type ComponentPropsType<T = {}> = {
+  className?: string;
+  children?: ReactNode;
+} & T;
