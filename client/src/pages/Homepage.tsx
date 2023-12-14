0709ed188ebc6bd6acf26653adc3e947b768f074
Randriamananjara Nirinasoa Herilanto <nirinasoa.herilanto@gmail.com>
2023-12-14 19:06:14 +0300

    update["client"]: working on landing page

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,30 +1,55 @@
-import { useAppStore } from '@project/store/use-app-store';
+import { Fragment } from 'react';
+
+import { AboutUs, Hero, Summary, Testimonials } from '@project/components';
+import { IReview } from '@project/utils';
+
+const reviews: IReview[] = [
+  {
+    _id: 'review_01',
+    username: 'John Doe',
+    image: {
+      url: 'https://cdn.pixabay.com/photo/2016/11/29/21/58/white-male-1871406_1280.jpg',
+      alt: 'user image',
+    },
+    company: ' -- Inc',
+    rating: 4.5,
+    opinion:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dictum pretium odio, nec interdum massa cursus pulvinar. Sed condimentum ligula vel pulvinar dignissim. Quisque pretium eros tellus, vel sollicitudin mauris dictum ac.',
+  },
+  {
+    _id: 'review_02',
+    username: 'John Doe',
+    image: {
+      url: 'https://cdn.pixabay.com/photo/2016/11/29/21/58/white-male-1871406_1280.jpg',
+      alt: 'user image',
+    },
+    company: ' -- Inc',
+    rating: 4.5,
+    opinion:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dictum pretium odio, nec interdum massa cursus pulvinar. Sed condimentum ligula vel pulvinar dignissim. Quisque pretium eros tellus, vel sollicitudin mauris dictum ac.',
+  },
+  {
+    _id: 'review_03',
+    username: 'John Doe',
+    image: {
+      url: 'https://cdn.pixabay.com/photo/2016/11/29/21/58/white-male-1871406_1280.jpg',
+      alt: 'user image',
+    },
+    company: ' -- Inc',
+    rating: 4.5,
+    opinion:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dictum pretium odio, nec interdum massa cursus pulvinar. Sed condimentum ligula vel pulvinar dignissim. Quisque pretium eros tellus, vel sollicitudin mauris dictum ac.',
+  },
+];
 
 const Homepage = () => {
-  const {
-    theme: { currentTheme, displayThemeHandler },
-  } = useAppStore();
-
   return (
-    <div>
-      <h1>Welcome to My workspace</h1>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-      </ul>
-
-      {currentTheme !== 'light' && (
-        <button onClick={displayThemeHandler.bind(null, 'light')}>
-          Light mode
-        </button>
-      )}
-
-      {currentTheme !== 'dark' && (
-        <button onClick={displayThemeHandler.bind(null, 'dark')}>
-          Dark mode
-        </button>
-      )}
-    </div>
+    <Fragment>
+      <Hero />
+      <Summary />
+      <AboutUs />
+      <Testimonials reviews={reviews} />
+    </Fragment>
   );
 };
 
