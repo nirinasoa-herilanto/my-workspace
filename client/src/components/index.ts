cfbbb6daefe7ec31627054786a2c96f29bdacbbe
Randriamananjara Nirinasoa Herilanto <nirinasoa.herilanto@gmail.com>
2023-12-14 19:04:51 +0300

    re-export paths

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,6 +1,20 @@
 // Layout components
 export { default as RootLayout } from './layouts/RootLayout';
 
-// Section components
+// Atoms components
+export { default as Button } from './atoms/Button';
+export { default as Image } from './atoms/Image';
+export { default as Logo } from './atoms/Logo';
 
-// UI components
+// Molecules components
+export { default as ReviewItem } from './molecules/ReviewItem';
+export { default as FooterNav } from './molecules/FooterNav';
+export { default as CustomLink } from './molecules/CustomLink';
+
+// Organisms components
+export { default as Hero } from './organisms/Hero';
+export { default as Summary } from './organisms/Summary';
+export { default as AboutUs } from './organisms/AboutUs';
+export { default as Testimonials } from './organisms/Testimonials';
+export { default as Reviews } from './organisms/Reviews';
+export { default as Footer } from './organisms/Footer';
--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,1 +1,3 @@
 export * from './app.config';
+export * from './wording.config';
+export * from './footer.config';
