90fde1f65d0a4795e40ff4df9e02d6e2dd10f7fa
Randriamananjara Nirinasoa Herilanto <nirinasoa.herilanto@gmail.com>
2023-12-14 19:03:27 +0300

    feat["client"]: add new app config
    - footer config
    - wording config

---
+++ client/src/config/footer.config.ts
@@ -0,0 +1,5 @@
+export const footerConfig = {
+  phoneNumber: '+261 38 17 645 87',
+  email: 'nirinasoa.herilanto@gmail.com',
+  linkedInAccount: 'https://www.linkedin.com/in/nirinasoa-herilanto',
+};
