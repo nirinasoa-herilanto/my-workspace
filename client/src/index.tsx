66514c1d2f562dbb2fe39829cdae9b87af6a1270
Randriamananjara Nirinasoa Herilanto <nirinasoa.herilanto@gmail.com>
2023-12-14 19:05:23 +0300

    feat["client"]: add lazy loading package

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClientProvider } from '@tanstack/react-query';
 
+import { GlobalStyle, ThemeProvider } from '@project/styles';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
 import { queryClient } from '@project/utils';
 
-import MainRouter from './MainRouter.tsx';
-import { GlobalStyle, ThemeProvider } from '@project/styles';
 import AppStoreProvider from '@project/store';
+
+import MainRouter from './MainRouter.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
