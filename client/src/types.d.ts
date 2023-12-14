4ff862a4c3ceafdcac935aaca733eaf4cc859e1b
Randriamananjara Nirinasoa Herilanto <nirinasoa.herilanto@gmail.com>
2023-12-14 18:55:22 +0300

    update["client"]: add image type & refactor

--- client/src/types.d.ts
+++ client/src/types.d.ts
@@ -3,27 +3,57 @@ import { ReactNode } from 'react';
 /* eslint-disable @typescript-eslint/ban-types */
 
 /**
- * ### CustomPropsType
- * A default component props type.
+ * ### CustomWrapperPropsType
+ * - Used to avoid `repetition` of declaring props `type` on each wrapper component.
  * 
- * - Used to avoid `repetition` of declaring props `type` on each component.
- * 
- * Usage example
+ * Usage example: `Button` component
  * ```jsx
-  import React from 'react';
-  import type { ComponentPropsType } from '@project/types';
+    import React from 'react';
+    import styled from 'styled-components';
+    import { HTMLMotionProps, motion } from 'framer-motion';
 
-  const Layout: React.FC<ComponentPropsType> = ({ className, children }) => {
-    return (<div className={`layout ${className || ''}`}>
-      {children}
-    </div>);
-  };
+    import type { CustomWrapperPropsType } from '@project/types';
 
-  export default Layout;
+    const Button: React.FC<CustomWrapperPropsType<HTMLMotionProps<'button'>>> = ({
+      className,
+      children,
+      ...rest
+    }) => {
+      return (
+        <ButtonWrapper className={`button ${className || ''}`} {...rest}>
+          {children}
+        </ButtonWrapper>
+      );
+    };
 
+    const ButtonWrapper = styled(motion.button)`
+      &.button {
+        padding: 10px;
+        font-size: 16px;
+        color: var(--white);
+        font-family: 'Nunito', sans-serif;
+
+        border: none;
+        outline: none;
+        border-radius: 20px;
+        background: var(--blue-500);
+        box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.5);
+        cursor: pointer;
+      }
+    `;
+
+    export default Button;
  * ```
  */
-declare type CustomPropsType<T = {}> = {
+declare type CustomWrapperPropsType<T = {}> = {
   className?: string;
-  children?: ReactNode;
+  children: ReactNode;
 } & T;
+
+/**
+ * ### Image type
+ */
+declare type ImageType = {
+  url: string;
+  alt: string;
+};
