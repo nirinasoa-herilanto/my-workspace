3c43947569139441fe8a49068720e2bf09e6373d
Randriamananjara Nirinasoa Herilanto <nirinasoa.herilanto@gmail.com>
2023-12-18 19:03:34 +0300

    feat["client"]: add firebase config

--- client/src/config/app.config.ts
+++ client/src/config/app.config.ts
@@ -1,4 +1,5 @@
 export const appConfig = {
   apiEndpoints: import.meta.env.VITE_API_ENPOINTS || '',
   graphqlApiEndpoints: import.meta.env.VITE_GRAPHQL_ENDPOINTS || '',
+  firebaseLinkRedirection: import.meta.env.VITE_FIREBASE_LINK_REDIRECTION || '',
 };
--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,3 +1,5 @@
 export * from './app.config';
 export * from './wording.config';
 export * from './footer.config';
+export * from './app.config';
+export * from './firebase.config';
