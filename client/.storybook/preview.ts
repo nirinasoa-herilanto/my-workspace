fc8cbaa6f4994860944541b9568c088ebe9e41e4
Randriamananjara Nirinasoa Herilanto <nirinasoa.herilanto@gmail.com>
2023-12-28 11:18:59 +0300

    updade["client"]: Styled components in Storybook

--- client/.storybook/preview.ts
+++ client/.storybook/preview.ts
@@ -1,4 +1,21 @@
 import type { Preview } from '@storybook/react';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+import { ThemeProvider } from 'styled-components';
+
+import { lightMode, darkMode } from '../src/styles/themes';
+import GlobalStyles from '../src/styles/GlobalStyle';
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    themes: {
+      light: lightMode,
+      dark: darkMode,
+    },
+    defaultTheme: 'light',
+    Provider: ThemeProvider,
+    GlobalStyles,
+  }),
+];
 
 const preview: Preview = {
   parameters: {
